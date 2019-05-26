280e267bd5b82878901be468c586380050e969a6
century <name@qq.com>
2019-05-26 22:40:16 +0800

    dev2

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', CartInfoView.as_view(), name='show'),  # 购物车页面显示
     url(r'^update$', CartUpdateView.as_view(), name='update'),  # 购物车记录更新
     url(r'^delete$', CartDeleteView.as_view(), name='delete')  # 购物车记录删除
-]
+] 
+# 测试
